9793126816dea6b7e00bab29c1284b24136bb399
Zunino <Zuninojuanbautista@gmail.com>
2024-09-11 19:59:44 -0300

    feat: add navbar & main sections

--- js/scroll.js
+++ js/scroll.js
@@ -1,14 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const navbar = document.querySelector('nav');
+const navbar = document.querySelector('nav');
 
-    const floatingNav = () => {
-        if (window.scrollY > window.innerHeight * 0.1) {
-            navbar.classList.add('navbar-float');
-        } else {
-            navbar.classList.remove('navbar-float');
-        }
-    };
+const floatingNav = () => {
+    if (window.scrollY > window.innerHeight * 0.1) {
+        navbar.classList.add('navbar-float');
+    } else {
+        navbar.classList.remove('navbar-float');
+    }
+};
 
-    navbar.addEventListener('mouseover', floatingNav);
-    window.addEventListener('scroll', floatingNav);
-});
+window.addEventListener('scroll', floatingNav);
